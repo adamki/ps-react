20be1062f8ba49d4516a9661898446137f35728b
Adamki <adajensen@gmail.com>
2017-12-21 14:36:31 -0800

    adds Label component and ExampleLabel

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,3 @@
 export { default as ProgressBar } from './ProgressBar';
+export { default as Label } from './Label';
+export { default as HelloWorld } from './HelloWorld';
---
+++ src/docs/examples/Label/ExampleLabel.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Label from 'ps-react/Label';
+
+export default function ExampleLabel() {
+  return <Label htmlFor='test' label='test label' required={true} />
+}
